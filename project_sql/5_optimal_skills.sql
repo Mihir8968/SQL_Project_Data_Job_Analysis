d104aed523709d3650c894992a8dda63bb32f269
Mihir Anand <mihir8968@gmail.com>
2026-05-27 11:41:19 +0530

    update optimal skills

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -1,3 +1,6 @@
+/*
+This query returns an optimal list of 20 skills with average salary being priority followed by demand such that each skill is demanded in atleast 10 different jobs
+*/
 SELECT skills_dim.skill_id,
     skills_dim.skills,
     COUNT(skills_job_dim.job_id) AS demand_count,
@@ -5,8 +8,8 @@ SELECT skills_dim.skill_id,
 FROM job_postings_fact
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE job_title_short = 'Data Analyst'
-    AND salary_year_avg IS NOT NULL
+WHERE 
+    salary_year_avg IS NOT NULL
     AND job_work_from_home = TRUE
 GROUP BY skills_dim.skill_id,
     skills_dim.skills
